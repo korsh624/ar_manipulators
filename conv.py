5adad5347ccc3d2a6cbb121743f45ecd96be890f
korsh624 <korsh624@gmail.com>
2025-03-24 16:56:31 +0300

    add conveyor

--- conv.py
+++ conv.py
@@ -1,7 +1,7 @@
 import socket
 import time
 
-class Conveyer():
+class ConveyerLine():
     def __init__(self, ip:str, port:int) -> None:
         self.ip=ip
         self.port=port
@@ -12,4 +12,17 @@ class Conveyer():
     def setSpeed(self,s):
         self.UDPClientSocket.sendto(str.encode("r#"), self.serverAddressPort)
         self.UDPClientSocket.sendto(str.encode(f"s:{s}#"), self.serverAddressPort)
+        time.sleep(5)
+
+class ConveyerMuver():
+    def __init__(self, ip:str, port:int) -> None:
+        self.ip=ip
+        self.port=port
+        self.UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.serverAddressPort = (ip, port)
+
+
+    def setSpeed(self,point):
+        self.UDPClientSocket.sendto(str.encode("r#"), self.serverAddressPort)
+        self.UDPClientSocket.sendto(str.encode(f"s:1:{point}#"), self.serverAddressPort)
         time.sleep(5)
--- test2.py
+++ test2.py
@@ -5,11 +5,10 @@ UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 serverAddressPort = ("192.168.42.49", 8888)
 
 
-def setConveer(speed):
+def setConveer(tik):
     UDPClientSocket.sendto(str.encode("r#"), serverAddressPort)
-    UDPClientSocket.sendto(str.encode(f"s:{speed}#"), serverAddressPort)
+    UDPClientSocket.sendto(str.encode(f"s:{tik}#"), serverAddressPort)
 #
 # setConveer(150)
 # time.sleep(20)
 setConveer(0)
-time.sleep(2)
