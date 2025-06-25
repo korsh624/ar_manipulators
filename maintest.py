34dadc442db2b462fde3bff6e2f0f3f9dd4e0126
korsh624 <korsh624@gmail.com>
2025-06-25 13:59:57 +0300

    add variative manip

--- maintest.py
+++ maintest.py
@@ -2,13 +2,8 @@ from manipulator import Manipulator
 from conv import ConveyerLine, ConveyerMuver
 import time
 from lamp import Lamp
-angle=Manipulator("192.168.100.47", 8888)
-time.sleep(3)
-angle.toPoint(200,0,200,180,0)
-time.sleep(5)
-angle.toPoint(200,50,200,0,1)
-time.sleep(3)
-angle.toPoint(200,0,200,180,0)
-time.sleep(5)
-angle.toPoint(200,50,200,0,1)
+angle=Manipulator("192.168.100.47", 8888,"g")
+time.sleep(2)
+angle.toPoint(150,20,200,0,0)
+
 
--- manipulator.py
+++ manipulator.py
@@ -2,14 +2,18 @@ import socket
 import time
 
 class Manipulator():
-    def __init__(self, ip:str, port:int) -> None:
+    def __init__(self, ip:str, port:int, type:str) -> None:
         self.ip=ip
         self.port=port
         self.UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.type=type
         self.serverAddressPort = (ip, port)
 
 
     def toPoint(self,x, y, z, a=0, g=0):
         self.UDPClientSocket.sendto(str.encode("r#"), self.serverAddressPort)
-        self.UDPClientSocket.sendto(str.encode(f"g:{x}:{y}:{a}:{z}:{g}#"), self.serverAddressPort)
+        if self.type=="g":
+            self.UDPClientSocket.sendto(str.encode(f"{self.type}:{x}:{y}:{a}:{z}:{g}#"), self.serverAddressPort)
+        if self.type=="p":
+            self.UDPClientSocket.sendto(str.encode(f"{self.type}:{x}:{y}:{z}:{a}:{g}#"), self.serverAddressPort)
         time.sleep(5)
